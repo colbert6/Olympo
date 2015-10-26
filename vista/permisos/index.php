c4ddae16f9d1e5f8ec6d15a02923cb52db458c05
colbert6 <colbersiho_6696@hotmail.com>
2015-10-26 09:09:21 -0500

    movil

--- vista/permisos/index.php
+++ vista/permisos/index.php
@@ -1,45 +1,49 @@
 <script> url = <?php echo BASE_URL ?></script>
 
-<div class="navbar-inner text-center">
-<table align="center">
-    <tr>
-        <td><label>Perfil:</label></td>
-        <td>
-            <select required name="perfil" id="perfil">
-                <option value="0">Seleccione...</option>
-                <?php for($i=0;$i<count($this->datos_perfiles);$i++){ ?>
-                    <option value="<?php echo $this->datos_perfiles[$i]['ID_PERFIL_USUARIO'] ?>"><?php echo $this->datos_perfiles[$i]['DESCRIPCION'] ?></option>
-                <?php } ?>
-            </select>
-        </td>
-        <td id="celda_aceptar">
-            <a href="<?php echo BASE_URL?>permisos" class="btn btn-primary">Aceptar</a>
-        </td>
-    </tr>
-</table>
+
+<div >
+    <div class="row" align="center">
+        <div class="col-md-3">
+        <label label class="control-label col-sm-3" for="apellido_paterno">Perfil:</label>
+        </div>
+        <div class="col-md-3">
+                <select required name="perfil" id="perfil">
+                    <option value="0">Seleccione...</option>
+                    <?php for($i=0;$i<count($this->datos_perfiles);$i++){ ?>
+                        <option value="<?php echo $this->datos_perfiles[$i]['ID_PERFIL_USUARIO'] ?>"><?php echo $this->datos_perfiles[$i]['DESCRIPCION'] ?></option>
+                    <?php } ?>
+                </select>
+        </div>
+        <div class="col-md-3">
+            <div id="celda_aceptar">
+                <a href="<?php echo BASE_URL?>permisos" class="btn btn-success">Aceptar</a>
+            </div>
+        </div>
+    
+    </div>
  
 
 
-<div id="div_modulos" align="left">
-    <ul id='modulos'>
+<div class="row" id="div_modulos" align="center">
+    <div id='modulos'>
     <?php
         for($i=0;$i<count($this->datos_modulos);$i++){
             $idmodulo=$this->datos_modulos[$i]['ID_MODULO'];
             $idmodulo_padre=$this->datos_modulos[$i]['ID_PADRE'];
             $modulo=$this->datos_modulos[$i]['NOMBRE'];
             if($idmodulo_padre==0){
-                echo "<li>".$modulo."<ul>";
+                echo "<div class='row'>".$modulo."<div class='row'>";
                 for($j=0;$j<count($this->datos_modulos);$j++){
                     if($this->datos_modulos[$j]['ID_PADRE']==$idmodulo){
                         $id=$this->datos_modulos[$j]['ID_MODULO'];
                         $descripcion=$this->datos_modulos[$j]['NOMBRE'];
-                        echo "<li><label class='checkbox'><input type='checkbox' name='$id' id='$id'/>".$descripcion."</label></li>";
+                        echo "<div class='col-md-4'><label class='checkbox'><input type='checkbox' name='$id' id='$id'/>".$descripcion."</label></div>";
                     }
                 }                
-                echo "</ul></li>";
+                echo "</div></div>";
             }
         }
     ?>
-    </ul>
+    </d>
 </div>
 
