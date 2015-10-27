d0660d1d7b0143efab5280f0ca1b48cfa24b155c
colbert6 <colbersiho_6696@hotmail.com>
2015-10-27 10:35:08 -0500

    modulo almacen

--- controlador/perfiles_controlador.php
+++ controlador/perfiles_controlador.php
@@ -2,35 +2,35 @@
 
 class perfiles_controlador extends controller {
 
-    private $_perfiles;
+    private $_model;
 
     public function __construct() {
         if (!$this->acceso()) {
             $this->redireccionar('error/access/5050');
         }
         parent::__construct();
-        $this->_perfiles = $this->cargar_modelo('perfiles');
+        $this->_model = $this->cargar_modelo('perfiles');
     }
 
     public function index() {
         $this->_vista->titulo = 'Lista de Perfiles';
         $this->_vista->datos = $this->_perfiles->selecciona();
-        $this->_vista->setJs(array('funcion'));
-        $this->_vista->setJs_Foot(array('scriptgrilla'));
+        $this->_vista->setCss_public(array('jquery.dataTables'));
+        $this->_vista->setJs_public(array('jquery.dataTables.min','run_table'));
         $this->_vista->renderizar('index');
     }
     
     public function buscador(){
         if($_POST['filtro']==0){
-            $this->_perfiles->descripcion=$_POST['descripcion'];
+            $this->_model->descripcion=$_POST['descripcion'];
         }
-        echo json_encode($this->_perfiles->selecciona());
+        echo json_encode($this->_model->selecciona());
     }
     
     public function nuevo() {
         if ($_POST['guardar'] == 1) {
-            $this->_perfiles->descripcion = $_POST['descripcion'];
-            $this->_perfiles->inserta();
+            $this->_model->descripcion = $_POST['descripcion'];
+            $this->_model->inserta();
             $this->redireccionar('perfiles');
         }
         $this->_vista->titulo = 'Registrar Perfil';
@@ -44,13 +44,13 @@ class perfiles_controlador extends controller {
             $this->redireccionar('perfiles');
         }
 
-        $this->_perfiles->idperfil = $this->filtrarInt($id);
+        $this->_model->idperfil = $this->filtrarInt($id);
         $this->_vista->datos = $this->_perfiles->selecciona();
 
         if ($_POST['guardar'] == 1) {
-            $this->_perfiles->idperfil = $_POST['codigo'];
-            $this->_perfiles->descripcion = $_POST['descripcion'];
-            $this->_perfiles->actualiza();
+            $this->_model->idperfil = $_POST['codigo'];
+            $this->_model->descripcion = $_POST['descripcion'];
+            $this->_model->actualiza();
             $this->redireccionar('perfiles');
         }
         $this->_vista->titulo = 'Actualizar Perfil';
@@ -62,8 +62,8 @@ class perfiles_controlador extends controller {
         if (!$this->filtrarInt($id)) {
             $this->redireccionar('perfiles');
         }
-        $this->_perfiles->idperfil = $this->filtrarInt($id);
-        $this->_perfiles->elimina();
+        $this->_model->idperfil = $this->filtrarInt($id);
+        $this->_model->elimina();
         $this->redireccionar('perfiles');
     }
 
