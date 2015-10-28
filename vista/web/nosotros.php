0e75f85a98d6db5efc8734a394915497b8d0754f
Jpoeta95 <yon_keynes95@hotmail.com>
2015-10-28 10:19:01 -0500

    web

--- vista/web/nosotros.php
+++ vista/web/nosotros.php
@@ -1,13 +1,16 @@
   <section  class="row">
+    <h2 class="col-lg-offset-1"><strong>BIENVENIDOS</strong></h2>
+                <hr>
         <div class="row">
-            <div class="col-sm-12 media-right media-left">
-                <h2 class="col-lg-offset-5">BIEMBENIDOS</h2>
+            <div class="col-sm-8 media-right media-left">
+                
                 <p class="text-justify">
                    <a class="pull-left" href="#">
-                        <img class="img-thumbnail" src="<?php echo $_webParams['ruta_img']; ?>web/bienvenida.jpg" alt="">
-                     </a>
-                    <div class="media-right media-left">
-                              
+                        <img class="img-thumbnail" src="<?php echo $_webParams['ruta_img']; ?>ima.jpg" alt="">
+                  </a>
+                </p>
+                <h5>  &nbsp;</h5>
+             <div class="media-right media-left">
                     <p class="text-justify">
                     La familia <strong>Olimpo Ginevra & Company Fitness</strong> te da 
                     la bienvenida a nuestra página web y al mismo tiempo te invita a formar parte de nuestra gran familia en donde te ayudaremos a cumplir tus metas y objetivos para mejorar tu estilo de vida de forma sana haciendo lo que más nos gusta,<strong> ¡DEPORTE!</strong>.</p>
@@ -17,174 +20,32 @@
                     </strong>  y disfruta de nuestros servicios de Cardiovascular,
                     Maquinas y pesas, Baile, Aeróbicos y mucho más, recuerda 
                     que tus metas son nuestras metas. Vive sano y saludable haciendo deporte.
-                    </p></p>
-                   </div>
-            
+                    </p>
+             </div>
+                              
             </div>
-            <div class="row">
-                <div class="col-sm-6 media-right media-left">
-                    <h2 class="col-lg-offset-3">MISION</h2>
+             <div class="col-sm-4 media-right media-left">
+                    <h5 class="col-lg-offset-1"><strong>MISION</strong></h5>
                     <p class="text-justify">
                         GInspirar a nuestros miembros inconparable energia para ayudarles a 
                         alcanzar sus objetivos individuales; con nuestra amplia experiencia les preeveemos bienestar
                         en base a un esmerado servicio, a un ambiente agradabre y con un personal entrenado en los ultimos
                         conocimientos disponibles.</p>
                 </div>
-                <div class="col-sm-6 media-right media-left">
-                    <h2 class="col-lg-offset-3">VISION</h2>
+                <div class="col-sm-4 media-right media-left">
+                    <h5 class="col-lg-offset-1"><strong>VISION</strong></h5>
                     <p class="text-justify">
                         Ser el mejor Gimnasio de la Region brindando bienestar a nuestos
                         mienbros y en general a la poblacion, generando valor a nuestra empresa, a nuestros colaboradores
                         y a nuestra comunidad.</p>
                 </div>
-            </div>
-            <div class="col-sm-6 media-right media-left">
-                <h2 class="col-lg-offset-3">Quienes somos</h2>
-                <p class="text-justify">GYM HUATULCO FITNESS CENTER es una empresa familiar con 
-                    grandes aspiraciones de fomentar la salud física y mental, 
-                    otorgando beneficios a su vida diaria; contamos con instructores 
-                    capacitados, con años de experiencia en la materia, brindamos también
-                    orientación y atención de médicos generales, especialistas en traumatología
-                    con cédula profesional y servicio nutricional.
-                    Estamos comprometidos con  “mejorar la calidad de vida de las personas
-                    a través de la filosofía del ejercicio, infraestructura, programas, productos 
-                    y de inculcar en la vida de toda la comunidad el valor de la salud y el ejercicio.</p>
-            </div><!--/.col-sm-6-->
-            <div class="col-sm-6 media-right media-left">
-                <h2>  &nbsp;</h2>
-                 <div class="row carousel-holder">
-
-                    <div class="col-md-12">
-
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>web/1.png" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>web/2.png" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>web/3.jpg" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-            </div><!--/.col-sm-6-->
-            <div class="col-sm-6 media-right media-left">
-                <h2>  &nbsp;</h2>
-                 <div class="row carousel-holder">
-
-                    <div class="col-md-12">
-
-                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>web/1.png" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>web/2.png" alt="">
-                                </div>
-                                <div class="item">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>web/3.jpg" alt="">
-                                </div>
-                            </div>
-                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span class="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span class="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-            </div><!--/.col-sm-6-->
-            <div class="col-sm-6 media-right media-left">
-                <h2 class="col-lg-offset-3">NUESTRA HISTORIA</h2>
-                <p class="text-justify">GYM HUATULCO FITNESS CENTER es una empresa familiar con 
-                    grandes aspiraciones de fomentar la salud física y mental, 
-                    otorgando beneficios a su vida diaria; contamos con instructores 
-                    capacitados, con años de experiencia en la materia, brindamos también
-                    orientación y atención de médicos generales, especialistas en traumatología
-                    con cédula profesional y servicio nutricional.
-                    Estamos comprometidos con  “mejorar la calidad de vida de las personas
-                    a través de la filosofía del ejercicio, infraestructura, programas, productos 
-                    y de inculcar en la vida de toda la comunidad el valor de la salud y el ejercicio.</p>
-            </div><!--/.col-sm-6-->
+                
+            
+                
+            
+            
+            <!--/.col-sm-6-->
             
         </div><!--/.row-->
 
-        <div class="gap"></div>
-        <h1 class="text-center">Conocer al equipo</h1>
-        <p class="text-center">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
-        <div class="gap"></div>
         
-    
-    <div class="contenedor " >
-
-                    <div class="col-sm-3 col-lg-3 col-md-3">
-                        <div class="img-thumbnail_1 center-block">
-                            <img class=" center-block img-circle" src="<?php echo $_webParams['ruta_img']; ?>web/images.jpg" alt="">
-                            <div class="caption text-center">
-                                <h4><a href="#">Servicios</a></h4>
-                                <h5>David J. Robbins Senior Vice President</h5>
-                                <p>Morbi accumsan ipsum velit. .</p>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    
-                    <div class="col-sm-3 col-lg-3 col-md-3">
-                        <div class="img-thumbnail_1 center-block">
-                            <img class=" center-block img-circle" src="<?php echo $_webParams['ruta_img']; ?>web/images.jpg" alt="">
-                            <div class="caption text-center">
-                                <h4><a href="#">Servicios</a></h4>
-                                <h5>David J. Robbins Senior Vice President</h5>
-                                <p>Morbi accumsan ipsum velit. .</p>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    
-                    <div class="col-sm-3 col-lg-3 col-md-3">
-                        <div class="img-thumbnail_1 center-block">
-                            <img class=" center-block img-circle" src="<?php echo $_webParams['ruta_img']; ?>web/images.jpg" alt="">
-                            <div class="caption text-center">
-                                <h4><a href="#">Servicios</a></h4>
-                                <h5>David J. Robbins Senior Vice President</h5>
-                                <p>Morbi accumsan ipsum velit. .</p>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    
-                    <div class="col-sm-3 col-lg-3 col-md-3">
-                        <div class="img-thumbnail_1 center-block">
-                            <img class=" center-block img-circle" src="<?php echo $_webParams['ruta_img']; ?>web/images.jpg" alt="">
-                            <div class="caption text-center">
-                                <h4><a href="#">Servicios</a></h4>
-                                <h5>David J. Robbins Senior Vice President</h5>
-                                <p>Morbi accumsan ipsum velit. .</p>
-                            </div>
-                            
-                        </div>
-                    </div>
