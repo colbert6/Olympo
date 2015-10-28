b29e2fbee7e4bd080ffeb3ca705e86e636c25059
colbert6 <colbersiho_6696@hotmail.com>
2015-10-28 15:26:55 -0500

    error

--- vista/web/index.php
+++ vista/web/index.php
@@ -1,4 +1,4 @@
-<div class="col-md-9">
+    </div><div class="col-md-9">
 
                 <div class="row carousel-holder">
 
@@ -12,13 +12,13 @@
                             </ol>
                             <div class="carousel-inner">
                                 <div class="item active">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>1.png" alt="">
+                                    <img class="slide-image"  src="<?php echo $_webParams['ruta_img']; ?>img.jpg" alt="">
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>2.png" alt="">
+                                    <img class="slide-image " src="<?php echo $_webParams['ruta_img']; ?>3.jpg" alt="">
                                 </div>
                                 <div class="item">
-                                    <img class="slide-image" src="<?php echo $_webParams['ruta_img']; ?>3.jpg" alt="">
+                                    <img class="slide-image " src="<?php echo $_webParams['ruta_img']; ?>2.png" alt="">
                                 </div>
                             </div>
                             <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
@@ -35,9 +35,9 @@
                                  
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                           <div class="hover-bg">
+                           <div class="hover-bg img-thumbnail">
                             <div class="hover-text">
-                                   <a href="<?php echo BASE_URL.'web/productos' ; ?>"> <h4>Productos</h4>
+                                   <a href="<?php echo BASE_URL.'productos' ; ?>"> <h4>Productos</h4>
                                     <h5>Ofrecemos productos pensado en sus objetivos, cardio,
                                         tonificacion,etc. ademas de asesoria por expertos </h5>
                                     <div class="clearfix"></div>
@@ -53,25 +53,35 @@
                         
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                            <div class="hover-bg">
-                                <iframe width="240" height="188"
-                                        src="https://www.youtube.com/embed/dLMPII2joCE"
-                                        frameborder="100" allowfullscreen>
-                                </iframe>
+                           <div class="hover-bg img-thumbnail">
+                            <div class="hover-text">
+                                   <a href="<?php echo BASE_URL.'nosotros' ; ?>"> <h4>Quienes Somos</h4>
+                                    <h5><p class="text-center">
+                                            La familia <strong>Olimpo Ginevra & Company Fitness</strong> 
+                                            te da la bienvenida a nuestra página web y al mismo tiempo te 
+                                            invita a formar parte de nuestra gran familia.. </h5>
+                                    <div class="clearfix"></div>
+                                    <i class="fa fa-plus"></i></a>
+                            </div>    
+                            <img src="<?php echo $_webParams['ruta_img']; ?>9.jpg" alt=""> 
+                            <div class="caption">
+                              <h4><a href="#">Quienes Somos</a></h4>  
+                            </div>
                            </div>
                         </div>
                     </div>
+
                     <div class="col-sm-4 col-lg-4 col-md-4">
                         <div class="thumbnail">
-                           <div class="hover-bg">
+                           <div class="hover-bg img-thumbnail">
                             <div class="hover-text">
-                                   <a href="<?php echo BASE_URL.'web/servicios' ; ?>"> <h4>Servicios</h4>
+                                   <a href="<?php echo BASE_URL.'servicios' ; ?>"> <h4>Servicio</h4>
                                     <h5>Ofrecemos servicios pensado en sus objetivos, cardio,
                                         tonificacion,etc. ademas de asesoria por expertos </h5>
                                     <div class="clearfix"></div>
                                     <i class="fa fa-plus"></i></a>
                             </div>    
-                            <img src="<?php echo $_webParams['ruta_img']; ?>4.jpg" alt=""> 
+                            <img src="<?php echo $_webParams['ruta_img']; ?>image.jpg" alt=""> 
                             <div class="caption">
                               <h4><a href="#">Servicios</a></h4>  
                             </div>
@@ -83,45 +93,32 @@
                      <div class="col-sm-12 col-lg-12 col-md-12" >
                         <ul class="nav nav-tabs" >
                             <li class="active"><a data-toggle="tab" href="#bienvenida">BIENVENIDA</a></li>
-                            <li ><a data-toggle="tab" href="#mision">MISION</a></li>
-                            
-                            <li><a data-toggle="tab" href="#vision">VISION</a></li>
-                          </ul>
+            
+                        </ul>
 
                         <div class="tab-content">
                             <div id="bienvenida" class="tab-pane fade in active">
                                <br>
                                     <div class="media">
                                       <a class="pull-left" href="#">
-                                        <img class="img-thumbnail" src="<?php echo $_webParams['ruta_img']; ?>bienvenida.jpg" alt="">
+                                        <img class="img-thumbnail " src="<?php echo $_webParams['ruta_img']; ?>bienvenida.jpg" alt="">
                                       </a>
                                       <div class="media-body">
                                         
                                         <p class="text-justify">
                                             La familia <strong>Olimpo Ginevra & Company Fitness</strong> te da la bienvenida a nuestra página web y al mismo tiempo te invita a formar parte de nuestra gran familia en donde te ayudaremos a cumplir tus metas y objetivos para mejorar tu estilo de vida de forma sana haciendo lo que más nos gusta,<strong> ¡DEPORTE!</strong>.</p>
                                           <p class="text-justify">
-                                            <strong>¡No lo pienses más!</strong> y únete a la familia <strong>Olimpo Ginevra & Company Fitness </strong>  y disfruta de nuestros servicios de Cardiovascular, Maquinas y pesas, Baile, Aeróbicos y mucho más, recuerda que tus metas son nuestras metas. Vive sano y saludable haciendo deporte.
+                                            <strong>¡No lo pienses más!</strong> y únete a la familia <strong>Olimpo Ginevra & Company Fitness </strong> no esperes mas y 
+                                            <a class="btn btn-warning " href="<?php echo BASE_URL.'contactenos' ; ?>"> contactenos a ..<i class="icon-chevron-right"></i></a>
+                                            
+
+                                
                                         </p>
                                       </div>
                                     </div>
                                         
                                     
                                    <br>                                 
-                            </div>
-                            <div id="mision" class="tab-pane fade">
-                                    <br/>
-                        
-                                    
-                                    <p class="text-justify">Inspirar a nuestros miembros inconparable energia para ayudarles a alcanzar sus objetivos individuales; con nuestra amplia experiencia les preeveemos bienestar en base a un esmerado servicio, a un ambiente agradabre y con un personal entrenado en los ultimos conocimientos disponibles.</p>
-
-                                    <br>
-                            </div>
-                            <div id="vision" class="tab-pane fade">
-                                <br/>
-                               
-                                <p class="text-justify">Ser el mejor Gimnasio de la Region brindando bienestar a nuestos mienbros y en general a la poblacion, generando valor a nuestra empresa, a nuestros colaboradores y a nuestra comunidad.</p>
-
-                                <br>
                             </div>
                         </div>
                     </div>
