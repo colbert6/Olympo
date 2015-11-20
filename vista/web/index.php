91fd79a6c1db02045a0f9bfc816d9841014f9672
colbert6 <colbersiho_6696@hotmail.com>
2015-11-19 21:47:25 -0500

    en web

--- vista/web/index.php
+++ vista/web/index.php
@@ -69,84 +69,28 @@
         </div>
 
         <?php if (isset($this->inicio) && count($this->inicio)){ ?>
-                <?php for($i = 0; $i < count($this->inicio); $i++){?> 
+            <?php for($i = 0; $i < count($this->inicio); $i++){?> 
                 <div class="col-sm-4 col-lg-4 col-md-4">
-                     <div class="thumbnail">
-                        <div class="hover-bg img-thumbnail">
-                          <div class="hover-text">
-                            <a href="<?php echo BASE_URL.'web/productos' ; ?>"> <h4 ><?php if(isset ($this->inicio[$i]['TITULO']))echo $this->inicio[$i]['TITULO']?></h4>
-                              <h5><?php if(isset ($this->inicio[$i]['DESCRIPCION']))echo $this->inicio[$i]['DESCRIPCION']?></h5>
-                                <div class="clearfix"></div>
-                                <i class="fa fa-plus"></i></a>
-                          </div>    
-                         <img src="<?php echo $_webParams['ruta_img']; ?><?php if(isset ($this->inicio[$i]['IMAGEN']))echo $this->inicio[$i]['IMAGEN']?>" alt=""> 
-                        <div class="caption">
-                           <h4><a href="#"><?php if(isset ($this->inicio[$i]['TITULO']))echo $this->inicio[$i]['TITULO']?></a></h4>  
+                    <div class="thumbnail">
+                        <div class="hover-bg">
+                            <div class="hover-text">
+                              <a href="<?php echo BASE_URL.'web/productos' ; ?>"> <h4 ><?php if(isset ($this->inicio[$i]['TITULO']))echo $this->inicio[$i]['TITULO']?></h4>
+                                <h5><?php if(isset ($this->inicio[$i]['DESCRIPCION']))echo $this->inicio[$i]['DESCRIPCION']?></h5>
+                                  <div class="clearfix"></div>
+                                  <i class="fa fa-plus"></i>
+                              </a>
+                            </div>    
+                            <img src="<?php echo $_webParams['ruta_img']; ?><?php if(isset ($this->inicio[$i]['IMAGEN']))echo $this->inicio[$i]['IMAGEN']?>" alt="" style="height: 90%;"> 
+                            <div class="caption">
+                                <h4 class="text-center"><?php if(isset ($this->inicio[$i]['TITULO']))echo $this->inicio[$i]['TITULO']?></h4>  
+                            </div>
                         </div>
-                      </div>
-                   </div>
+                    </div>
                </div>
 
-                <?php } ?> 
+            <?php } ?> 
         <?php } ?> 
                             
-        
-   <!--     <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-                <div class="hover-bg img-thumbnail">
-                    <div class="hover-text">
-                        <a href="<?php echo BASE_URL.'web/productos' ; ?>"> <h4 >Productos</h4>
-                        <h5>Ofrecemos productos pensado en sus objetivos, cardio,
-                            tonificacion,etc. ademas de asesoria por expertos </h5>
-                        <div class="clearfix"></div>
-                        <i class="fa fa-plus"></i></a>
-                    </div>    
-                    <img src="<?php echo $_webParams['ruta_img']; ?>5.jpg" alt=""> 
-                    <div class="caption">
-                      <h4><a href="#">Productos</a></h4>  
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-               <div class="hover-bg img-thumbnail">
-                <div class="hover-text">
-                       <a href="<?php echo BASE_URL.'web/nosotros' ; ?>"> <h4>Quienes Somos</h4>
-                        <h5><p class="text-center">
-                                La familia <strong>Olimpo Ginevra & Company Fitness</strong> 
-                                te da la bienvenida a nuestra página web y al mismo tiempo te 
-                                invita a formar parte de nuestra gran familia.. </h5>
-                        <div class="clearfix"></div>
-                        <i class="fa fa-plus"></i></a>
-                </div>    
-                <img src="<?php echo $_webParams['ruta_img']; ?>9.jpg" alt=""> 
-                <div class="caption">
-                  <h4><a href="#">Quienes Somos</a></h4>  
-                </div>
-               </div>
-            </div>
-        </div>
-
-        <div class="col-sm-4 col-lg-4 col-md-4">
-            <div class="thumbnail">
-               <div class="hover-bg img-thumbnail">
-                <div class="hover-text">
-                       <a href="<?php echo BASE_URL.'web/servicios' ; ?>"> <h4>Servicio</h4>
-                        <h5>Ofrecemos servicios pensado en sus objetivos, cardio,
-                            tonificacion,etc. ademas de asesoria por expertos </h5>
-                        <div class="clearfix"></div>
-                        <i class="fa fa-plus"></i></a>
-                </div>    
-                <img src="<?php echo $_webParams['ruta_img']; ?>image.jpg" alt=""> 
-                <div class="caption">
-                  <h4><a href="#">Servicios</a></h4>  
-                </div>
-               </div>
-            </div>
-        </div>-->
- 
         
         <div class="row">
             <div class="col-sm-12 col-lg-12 col-md-12" >
